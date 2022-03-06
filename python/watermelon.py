78c24aab3bddfc61771280eac732fb28ddc2dd0a
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-03-06 23:18:42 +0900

    Update watermelon.py

--- python/watermelon.py
+++ python/watermelon.py
@@ -1,14 +1,9 @@
 def solution(n):
     answer = ''
-    while(n>0):
-        if n%2==0:
+    for i in range(n):
+        if (i%2)==0:
             answer=answer+'수'
         else:
             answer=answer+'박'
-        n=n-1
-
-    print(answer)
 
     return answer
-
-solution(7)
