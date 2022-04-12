acdf903435c4a72ddb1e932ed6a0e52c9b532f7e
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-04-12 18:59:39 +0900

    Update repaint_chess.py

--- python/repaint_chess.py
+++ python/repaint_chess.py
@@ -1,41 +1,42 @@
 #파이썬 입력도 공부하자!!
 a, b = map(int, input().strip().split(' '))
 chess=[]
+answer=[]
 
 for _ in range(a):
     chess.append(input())
 
-# %이용해서 flag 없이 구현 가능
-def checkNum(x,y):
-    minN=-1
+# % 이용해서 flag 없이 구현 가능
+# chess[x+k][y+l] 좌표에서 x+k와 y+l 주의!!
+def checkNum_B(x,y):
     cnt=0
-    flag=True
     for k in range(8):
         for l in range(8):
-            if flag==True:
-                if chess[x + k][y + l]!='B':
-                    chess[x + k][y + l] = 'B'
+            if ((x+k)+(y+l))%2==0:
+                if chess[x+k][y+l]!='B':
                     cnt=cnt+1
-                    flag=False
-                elif chess[x + k][y + l]=='B':
-                    flag = False
-                elif l==7:
-                    flag = True
-
-            elif flag==False:
-                if chess[x + k][y + l]!='W':
-                    chess[x + k][y + l] = 'W'
+            elif ((x+k)+(y+l))%2==1:
+                if chess[x+k][y+l]!='W':
                     cnt=cnt+1
-                    flag=True
-                elif chess[x + k][y + l]=='W':
-                    flag = True
-                elif l==7:
-                    flag = False
+    return cnt
 
+def checkNum_W(x,y):
+    cnt=0
+    for k in range(8):
+        for l in range(8):
+            if ((x+k)+(y+l))%2==0:
+                if chess[x+k][y+l]!='W':
+                    cnt=cnt+1
+            elif ((x+k)+(y+l))%2==1:
+                if chess[x+k][y+l]!='B':
+                    cnt=cnt+1
+    return cnt
 
 
 #체스판 자르는거 -> 자르지말고 시작점 보내는 법 생각!!
 for i in range(a-7):
     for j in range(b-7):
-        checkNum(i,j)
+        answer.append(checkNum_B(i,j))
+        answer.append(checkNum_W(i,j))
 
+print(min(answer))
