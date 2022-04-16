8a3f28f6961a9d8f0e0958a65791479a56afecfc
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-04-17 04:19:40 +0900

    Add files via upload

---
+++ python/boost_camp_2.py
@@ -0,0 +1,9 @@
+def solution(param):
+    answer = []
+    return answer
+
+
+print(solution(["INT", "INT", "BOOL", "SHORT", "LONG"]))
+print(solution(["INT", "SHORT", "FLOAT", "INT", "BOOL"]))
+print(solution(["FLOAT", "SHORT", "BOOL", "BOOL", "BOOL", "INT"]))
+print(solution(["BOOL", "LONG", "SHORT", "LONG", "BOOL", "LONG", "BOOL", "LONG", "SHORT", "LONG", "LONG"]))
