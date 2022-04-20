ac3d10354094d92acf1c4ed99379654f93554099
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-04-20 19:19:37 +0900

    Update blackjack.py

--- python/blackjack.py
+++ python/blackjack.py
@@ -1,3 +1,4 @@
+#조합 이용해서 완전탐색 한 후 가장 작은 차이 합을 출력
 
 N, M= map(int,input().split())
 arr = list(map(int,input().split()))
@@ -8,9 +9,19 @@ def DFS(idx, list):
         tmp.append(list[:])
         return
 
-    for i in (idx, N):
+    #range 빼먹지 말기!
+    for i in range(idx, N):
         DFS(i+1, list+[arr[i]])
 
 #DFS에 넣는거 빈 배열
 DFS(0,[])
-print(tmp)
+
+min=300000
+ans_idx=-1
+
+for j in range(len(tmp)):
+    if sum(tmp[j])<=M and M-sum(tmp[j])<min:
+        min=M-sum(tmp[j])
+        ans_idx=j
+
+print(sum(tmp[ans_idx]))
