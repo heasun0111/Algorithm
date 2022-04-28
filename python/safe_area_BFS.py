97cf37a9b9859839f9ad0a0d97ed3194462a85bf
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-04-28 20:37:54 +0900

    Update safe_area_BFS.py

--- python/safe_area_BFS.py
+++ python/safe_area_BFS.py
@@ -1,45 +1,44 @@
-import copy
+from collections import deque
 
 N=int(input())
 area=[list(map(int, input().split())) for _ in range(N)]
+visited=[[0 for _ in range(N)] for _ in range(N)]
 #깊은 복사 말고 T/F 이용해서 메모리를 사용하는게 좋다!
-areaTmp=copy.deepcopy(area)
-
 
 minN=min(map(min,area))
 maxN=max(map(max,area))
 
+q=deque()
+
 def BFS(x,y,h):
     dx=[1,0,-1,0]
     dy=[0,1,0,-1]
-    
-
-
-def DFS(x,y,h):
-    dx=[1,0,-1,0]
-    dy=[0,1,0,-1]
-    areaTmp[x][y]=0
-    for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if 0<=nx<N and 0<=ny<N and areaTmp[nx][ny]>h:
-            areaTmp[nx][ny]=0
-            DFS(nx,ny,h)
+    visited[x][y]=1
+    q.append([x,y])
+    while q:
+        a,b=q.popleft()
+        visited[a][b] = 1
+        for i in range(4):
+            nx=a+dx[i]
+            ny=b+dy[i]
+            if 0<=nx<N and 0<=ny<N and visited[nx][ny]==0 and area[nx][ny]>h:
+                visited[nx][ny]=1
+                q.append([nx,ny])
 
 
 def findSafeArea(h):
     cnt=0
     for n in range(N):
         for m in range(N):
-            if areaTmp[n][m]>h:
-                DFS(n,m,h)
+            if area[n][m]>h and visited[n][m]==0:
+                BFS(n,m,h)
                 cnt=cnt+1
     return cnt
 
 
 answer=[]
-for k in range(minN, maxN):
-    areaTmp = copy.deepcopy(area)
+for k in range(0, maxN):
+    visited=[[0 for _ in range(N)] for _ in range(N)]
     tmp=findSafeArea(k)
     answer.append(tmp)
 
