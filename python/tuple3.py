ca43c580cbee0dd4af3b5f599dec9307e9ad9fbf
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-04-30 14:41:01 +0900

    Add files via upload

---
+++ python/tuple3.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+#빈도에 따라서 정렬하는 함수
+def frequency_sort(data):
+    rt_data = []
+    for d, c in Counter(data).most_common():
+        rt_data.append(d)
+    return rt_data
+
+
+def solution(s):
+    tmp=[]
+    for i in range(len(s)):
+        if not(s[i]=='{' or s[i]=='}'):
+            tmp.append(s[i])
+
+
+    tmp="".join(tmp)
+    tmp=tmp.split(",")
+    tmp=list(map(int,tmp))
+
+    answer=frequency_sort(tmp)
+    return answer
+
+# 매일 한 문제씩 풀기!!
+
+
+print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
+print(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"))
+#print(solution("{{20,111},{111}}"))
+#print(solution("{{123}}"))
+#print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
