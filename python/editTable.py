cdf5aef308fd64e2f48dc8386b6666dc3975703d
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-05-04 20:35:06 +0900

    ...ing

---
+++ python/editTable.py
@@ -0,0 +1,19 @@
+#원소 삭제 후, U/D생각
+def solution(n, k, cmd):
+    N=range(n)
+    trash=[]
+    idx=k
+    for i in range(len(cmd)):
+        tmp=cmd[i]
+        if len(tmp)==1:
+            if tmp=="C":
+
+
+            elif tmp=="Z":
+
+        else:
+            tmp=tmp.split()
+
+
+    answer = ''
+    return answer
