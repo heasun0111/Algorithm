83e092bc70b116982c42cbb8d090eaffbdcbfc1f
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-06-12 12:56:19 +0900

    Delete boost_camp_2.py

--- python/boost_camp_2.py
+++
@@ -1,9 +0,0 @@
-def solution(param):
-    answer = []
-    return answer
-
-
-print(solution(["INT", "INT", "BOOL", "SHORT", "LONG"]))
-print(solution(["INT", "SHORT", "FLOAT", "INT", "BOOL"]))
-print(solution(["FLOAT", "SHORT", "BOOL", "BOOL", "BOOL", "INT"]))
-print(solution(["BOOL", "LONG", "SHORT", "LONG", "BOOL", "LONG", "BOOL", "LONG", "SHORT", "LONG", "LONG"]))
