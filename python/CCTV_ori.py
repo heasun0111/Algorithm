9818f03055d26e0fc328877c99891c8c6aee7590
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-06-28 19:22:09 +0900

    success!!

--- python/CCTV_ori.py
+++ python/CCTV_ori.py
@@ -1,10 +1,5 @@
 # 사각지대 개수 카운팅
 # CCTV 모든 경우의 수 #으로 출력
-# CCTV
-
-# CCTV 영역 넘어가는 거
-# CCTV5 제대로 작동 X
-# 모든 경우의 수 탐색 X
 
 from collections import deque
 import copy
@@ -18,9 +13,12 @@ y=[0,1,0,-1]
 def CCTV1(room, dx, dy, num):
     nx=dx
     ny=dy
-    while 0<=nx<M-1 and 0<=ny<N-1 and room[ny][nx]!=6:
+    while room[ny][nx]!=6:
         nx=nx+x[num]
         ny=ny+y[num]
+        if nx<0 or nx>=M or ny<0 or ny>=N:
+            break
+
         if room[ny][nx]==0:
             room[ny][nx]=9
         elif room[ny][nx]==6:
@@ -35,9 +33,11 @@ def CCTV2(room, dx, dy, num):
         nx = dx
         ny = dy
         dir=dir+num
-        while 0 <= nx < M-1 and 0 <= ny < N-1 and room[ny][nx] != 6:
+        while room[ny][nx] != 6:
             nx = nx + x[dir]
             ny = ny + y[dir]
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                break
             if room[ny][nx] == 0:
                 room[ny][nx] = 9
             elif room[ny][nx] == 6:
@@ -51,11 +51,13 @@ def CCTV3(room, dx, dy, num):
         nx = dx
         ny = dy
         dir=dir+num
-        if dir>4:
-            dir=dir/4
-        while 0 <= nx < M-1 and 0 <= ny < N-1 and room[ny][nx] != 6:
+        if dir>=4:
+            dir=dir%4
+        while room[ny][nx] != 6:
             nx = nx + x[dir]
             ny = ny + y[dir]
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                break
             if room[ny][nx] == 0:
                 room[ny][nx] = 9
             elif room[ny][nx] == 6:
@@ -69,11 +71,14 @@ def CCTV4(room, dx, dy, num):
         nx=dx
         ny=dy
         dir=dir+num
-        if dir>4:
-            dir=dir/4
-        while 0 <= nx < M-1 and 0 <= ny < N-1 and room[ny][nx] != 6:
+        #부등호에서 등호(범위) 제대로 체크!!
+        if dir>=4:
+            dir=dir%4
+        while room[ny][nx] != 6:
             nx = nx + x[dir]
             ny = ny + y[dir]
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                break
             if room[ny][nx] == 0:
                 room[ny][nx] = 9
             elif room[ny][nx] == 6:
@@ -86,9 +91,11 @@ def CCTV5(room, dx, dy):
     for dir in range(4):
         nx = dx
         ny = dy
-        while 0 <= nx < M-1 and 0 <= ny < N-1 and room[ny][nx] != 6:
+        while room[ny][nx] != 6:
             nx = nx + x[dir]
             ny = ny + y[dir]
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                break
             if room[ny][nx] == 0:
                 room[ny][nx] = 9
             elif room[ny][nx] == 6:
@@ -127,6 +134,7 @@ for n in range(N):
 
 num4=len(CCTV_Point)
 num2=len(CCTV_Point2)
+num5=len(CCTV_Point5)
 
 arr4=[0,1,2,3]
 arr2=[0,1]
@@ -164,15 +172,24 @@ elif num4!= 0 and num2==0:
     for tmp in direction_4(arr4, num4):
         total.append(tmp)
 
-print(total)
 
 answer=100
+
+if len(CCTV_Point5) != 0:
+    for c in CCTV_Point5:
+        CCTV5(room, c[0], c[1])
+
+#리스트가 아예 0인 경우!!
+if len(total)==0:
+    answer=counting(room)
+
 for list_a in total:
     que=deque(list_a)
     room_tmp=copy.deepcopy(room)
     for a in CCTV_Point:
         if a[0]==1:
             CCTV1(room_tmp, a[1], a[2], que.popleft())
+
         elif a[0]==3:
             CCTV3(room_tmp, a[1], a[2], que.popleft())
         elif a[0]==4:
@@ -180,18 +197,20 @@ for list_a in total:
     for b in CCTV_Point2:
         CCTV2(room_tmp, b[0], b[1], que.popleft())
 
-    if CCTV_Point5!=0:
-        for c in CCTV_Point5:
-            CCTV5(room_tmp, c[0], c[1])
-
-    for oh in range(len(room_tmp)):
-        print(room_tmp[oh])
-    print(counting(room_tmp))
-    print(' ')
 
     if answer>counting(room_tmp):
         answer=counting(room_tmp)
 
+'''
+    for oh in range(len(room_tmp)):
+        print(room_tmp[oh])
+    print(counting(room_tmp))
+    print(' ')
+'''
 
 print(answer)
+
+# 내가 고려하지 못한 예외처리!!
+# CCTV가 없는 경우
+# CCTV5에 대한 처리
 
