3a310c279bc1b393fc65d2ff503ae08bd8269813
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-07-14 20:21:17 +0900

    Add files via upload

--- python/dessert_cafe.py
+++ python/dessert_cafe.py
@@ -3,39 +3,45 @@ import sys
 left=0
 right=0
 
-dx=[1, -1, -1, 1]
-dy=[1, 1, -1, -1]
+dx=[1, 1, -1, -1]
+dy=[1, -1, -1, 1]
 
 def cafe_route(left, right, cafe):
     answer=[]
     route=[]
     for i in range(N):
         for j in range(N):
-            if j-left<0 or j+right>N or i+left+right>N:
+            if j-left<0 or j+right>=N or i+left+right>=N:
                 continue
             else:
                 route.append(cafe[i][j])
+                t_i=i
+                t_j=j
                 for _ in range(right):
-                    i=i+dx[0]
-                    j=j+dx[0]
-                    route.append(cafe[i][j])
+                    t_i=t_i+dx[0]
+                    t_j=t_j+dy[0]
+                    route.append(cafe[t_i][t_j])
                 for _ in range(left):
-                    i=i+dx[1]
-                    j=j+dx[1]
-                    route.append(cafe[i][j])
+                    t_i=t_i+dx[1]
+                    t_j=t_j+dy[1]
+                    route.append(cafe[t_i][t_j])
                 for _ in range(right):
-                    i=i+dx[2]
-                    j=j+dx[2]
-                    route.append(cafe[i][j])
+                    t_i=t_i+dx[2]
+                    t_j=t_j+dy[2]
+                    route.append(cafe[t_i][t_j])
                 for _ in range(left):
-                    i=i+dx[3]
-                    j=j+dx[3]
-                    route.append(cafe[i][j])
+                    t_i=t_i+dx[3]
+                    t_j=t_j+dy[3]
+                    if t_i==i and t_j==j:
+                        break
+                    route.append(cafe[t_i][t_j])
 
                 set_route=list(set(route))
-                if len(route)==set_route:
+                if len(route)==len(set_route):
                     if len(answer)<len(route):
                         answer=route
+                route = []
+
     return answer
 
 
@@ -47,7 +53,10 @@ def brute(N, cafe):
             left=j
             if i-left>=1:
                 right=i-left
-                answer=cafe_route(left, right, cafe)
+                tmp=cafe_route(left, right, cafe)
+                if len(tmp)>len(answer):
+                    answer=tmp
+
             else:
                 continue
 
@@ -61,6 +70,6 @@ for TC in range(1, T+1):
     if len(ans)==0:
         print('#'+str(TC)+' '+str(-1))
     else:
-        print('#'+str(TC)+ ' ' +str(ans))
+        print('#'+str(TC)+ ' ' +str(len(ans)))
 
 
