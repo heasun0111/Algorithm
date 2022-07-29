914949e75942866c0b66b588bf72d11532c8bf2c
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-07-30 05:02:27 +0900

    Update runway.py

--- python/runway.py
+++ python/runway.py
@@ -1,8 +1,7 @@
-# 경사로 중복되는 경우 어케 처리할지 생각!
 
 N, L = map(int, input().split())
 runway=[list(map(int, input().split())) for _ in range(N)]
-visited=[[0 for _ in range(N)]for _ in range(N)]
+visited=[[0 for _ in range(N)] for _ in range(N)]
 
 ans=0
 for j in range(N):
@@ -11,49 +10,44 @@ for j in range(N):
         if abs(runway[j][i]-runway[j][i+1])>1:
             flag=False
             break
-
-        elif runway[j][i]-runway[j][i+1] == 1:
+        elif runway[j][i]-runway[j][i+1]==1 and visited[j][i]==0 and visited[j][i+1]==0:
             visited[j][i+1]=1
+            #방문 표시한거 다시 초기화
             for k in range(1,L):
                 if i+1+k>=N:
                     flag=False
                     break
-
-                if runway[j][i+1] != runway[j][i+1+k]:
-                    flag = False
+                if runway[j][i+1]!=runway[j][i+1+k]:
+                    flag=False
                     break
-
-                # 경사로 넣는거 구현!! -> 경우의 수 고려
-                elif visited[j][i+1]!=0 and visited[j][i+1+k]!=0:
-                    flag = False
-
                 else:
-                    visited[j][i+1+k]=k+1
+                    visited[j][i+1+k]=1
 
-
-        elif runway[j][i]-runway[j][i+1] == -1:
+        elif runway[j][i]-runway[j][i+1]==-1 and visited[j][i]==0 and visited[j][i+1]==0:
+            visited[j][i]=1
             for k in range(1,L):
-                if i-k < 0:
+                if i+1+k>=N:
                     flag=False
                     break
-                if runway[j][i] != runway[j][i-k]:
+                if runway[j][i+1]!=runway[j][i+1+k]:
                     flag=False
                     break
-
+                else:
+                    visited[j][i+1+k]=1
         elif runway[j][i]-runway[j][i+1]==0:
             continue
-
     if flag==True:
         ans=ans+1
 
 for j in range(N):
     flag = True
-    for i in range(N - 1):
+    for i in range(N-1):
         if abs(runway[i][j] - runway[i + 1][j]) > 1:
-            flag = False
+            flag=False
             break
 
-        elif runway[i][j] - runway[i + 1][j] == 1:
+        elif runway[i][j]-runway[i + 1][j]==1 and visited[j][i]==0 and visited[j][i+1]==0:
+            visited[j][i]=1
             for k in range(1, L):
                 if i + 1 + k >= N:
                     flag = False
@@ -61,8 +55,11 @@ for j in range(N):
                 if runway[i + 1][j] != runway[i + 1 + k][j]:
                     flag = False
                     break
+                else:
+                    visited[j][i+1+k]=1
 
-        elif runway[i][j] - runway[i + 1][j] == -1:
+        elif runway[i][j]-runway[i + 1][j]==-1  and visited[j][i]==0 and visited[j][i+1]==0:
+            visited[j][i] = 1
             for k in range(1, L):
                 if i - k < 0:
                     flag = False
@@ -70,11 +67,16 @@ for j in range(N):
                 if runway[i][j] != runway[i - k][j]:
                     flag = False
                     break
+                else:
+                    visited[j][i+1+k]=1
 
-        elif runway[i][j] - runway[i + 1][j] == 0:
+        elif runway[i][j]-runway[i + 1][j] == 0:
             continue
 
-    if flag == True:
-        ans = ans + 1
+    if flag==True:
+        ans=ans+1
+
+for j in range(N):
+    print(visited[j])
 
 print(ans)
