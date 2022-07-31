89d85054e5d7788328f0478998d03e3fde43ac4f
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-07-31 21:43:13 +0900

    Add files via upload

---
+++ python/snack.py
@@ -0,0 +1,14 @@
+
+M, N=map(int, input().split())
+
+snack=[]
+answer=0
+for _ in range(N):
+    tmp=int(input())
+    snack.append(tmp)
+
+flag=True
+if flag==True:
+    print(answer)
+else:
+    print('0')
