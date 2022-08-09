1f6a4f32686fc1f55539c46d9e99955ed315d7ce
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2022-08-09 19:58:23 +0900

    Update pinball.py

--- python/pinball.py
+++ python/pinball.py
@@ -1,4 +1,8 @@
 # 맵 밖으로 나가는 것을 5블록으로 감싸는 것으로 처리!
+'''
+A = [[5] * (N+2)]
+A.append([5] + list(map(int, input().split())) + [5])
+'''
 # 웜홀 딕셔너리 이용해서 양쪽으로 처리 -> 굳이 번호대로 입력할 필요 없다!
 # 굳이 블랙홀은 append 할 필요는 없을듯!
 
@@ -15,7 +19,6 @@ block=[
 ]
 
 def gameStart(game, i, j, d):
-    global black_hole
     global worm6
     global worm7
     global worm8
@@ -28,94 +31,80 @@ def gameStart(game, i, j, d):
     x=start_x+dir_x[d]
     y=start_y+dir_y[d]
     tmp_score=0
-    while (x!=start_x and y!=start_y) and ([x,y] not in black_hole):
-        if x<0:
-            x=0
-            d=(d+2)%4
+    while (x!=start_x or y!=start_y):
+        if game[x][y] == -1:
+            break
+
+        if game[x][y] == 0:
+            x = x + dir_x[d]
+            y = y + dir_y[d]
+
+        elif game[x][y] >= 1 and game[x][y] <= 5:
+            d = block[game[x][y]][d]
             tmp_score = tmp_score + 1
-        elif y<0:
-            y=0
-            d=(d+2)%4
-            tmp_score = tmp_score + 1
-        elif x>=N:
-            x=N-1
-            d=(d+2)%4
-            tmp_score = tmp_score + 1
-        elif y>=N:
-            y=N-1
-            d=(d+2)%4
-            tmp_score = tmp_score + 1
+            x = x + dir_x[d]
+            y = y + dir_y[d]
 
-        else:
-            if game[x][y] == 0:
-                x = start_x + dir_x[d]
-                y = start_y + dir_y[d]
+        elif game[x][y] == 6:
+            if worm6[0][0]==x and worm6[0][1]==y:
+                x=worm6[1][0]+dir_x[d]
+                y=worm6[1][1]+dir_y[d]
+            else:
+                x=worm6[0][0]+dir_x[d]
+                y=worm6[0][1]+dir_y[d]
 
-            elif game[x][y] == 1 or game[x][y] == 2 or game[x][y] == 3 or game[x][y] == 4 or game[x][y] == 5:
-                d = block[game[x][y]][d]
-                tmp_score = tmp_score + 1
-                x = start_x + dir_x[d]
-                y = start_y + dir_y[d]
+        elif game[x][y] == 7:
+            if worm7[0][0]==x and worm7[0][1]==y:
+                x=worm7[1][0]+dir_x[d]
+                y=worm7[1][1]+dir_y[d]
+            else:
+                x=worm7[0][0]+dir_x[d]
+                y=worm7[0][1]+dir_y[d]
 
-            elif game[x][y] == 6:
-                if worm6[0][0]==x and worm6[0][1]==y:
-                    x=worm6[1][0]+dir_x[d]
-                    y=worm6[1][1]+dir_y[d]
-                else:
-                    x=worm6[0][0]+dir_x[d]
-                    y=worm6[0][1]+dir_y[d]
+        elif game[x][y] == 8:
+            if worm8[0][0]==x and worm8[0][1]==y:
+                x=worm8[1][0]+dir_x[d]
+                y=worm8[1][1]+dir_y[d]
+            else:
+                x=worm8[0][0]+dir_x[d]
+                y=worm8[0][1]+dir_y[d]
 
-            elif game[x][y] == 7:
-                if worm7[0][0]==x and worm7[0][1]==y:
-                    x=worm7[1][0]+dir_x[d]
-                    y=worm7[1][1]+dir_y[d]
-                else:
-                    x=worm7[0][0]+dir_x[d]
-                    y=worm7[0][1]+dir_y[d]
+        elif game[x][y] == 9:
+            if worm9[0][0]==x and worm9[0][1]==y:
+                x=worm9[1][0]+dir_x[d]
+                y=worm9[1][1]+dir_y[d]
+            else:
+                x=worm9[0][0]+dir_x[d]
+                y=worm9[0][1]+dir_y[d]
 
-            elif game[x][y] == 8:
-                if worm8[0][0]==x and worm8[0][1]==y:
-                    x=worm8[1][0]+dir_x[d]
-                    y=worm8[1][1]+dir_y[d]
-                else:
-                    x=worm8[0][0]+dir_x[d]
-                    y=worm8[0][1]+dir_y[d]
-
-            elif game[x][y] == 9:
-                if worm9[0][0]==x and worm9[0][1]==y:
-                    x=worm9[1][0]+dir_x[d]
-                    y=worm9[1][1]+dir_y[d]
-                else:
-                    x=worm9[0][0]+dir_x[d]
-                    y=worm9[0][1]+dir_y[d]
-
-            elif game[x][y] == 10:
-                if worm10[0][0]==x and worm10[0][1]==y:
-                    x=worm10[1][0]+dir_x[d]
-                    y=worm10[1][1]+dir_y[d]
-                else:
-                    x=worm10[0][0]+dir_x[d]
-                    y=worm10[0][1]+dir_y[d]
+        elif game[x][y] == 10:
+            if worm10[0][0]==x and worm10[0][1]==y:
+                x=worm10[1][0]+dir_x[d]
+                y=worm10[1][1]+dir_y[d]
+            else:
+                x=worm10[0][0]+dir_x[d]
+                y=worm10[0][1]+dir_y[d]
 
     return tmp_score
 
 T = int(input())
 for tc in range(1, T + 1):
     N=int(input())
-    game=[list(map(int, input().split())) for _ in range(N)]
+    game=[[5]*(N+2)]
+    for _ in range(N):
+        game.append([5]+list(map(int, input().split()))+[5])
+    game.append([5]*(N+2))
+    # print문 이용해서 확인!!
 
-    black_hole=[]
     worm6=[]
     worm7=[]
     worm8=[]
     worm9=[]
     worm10=[]
 
-    for a in range(N):
-        for b in range(N):
-            if game[a][b]==-1:
-                black_hole.append([a,b])
-            elif game[a][b]==6:
+    for a in range(N+2):
+        for b in range(N+2):
+            if game[a][b]==6:
                 worm6.append([a,b])
             elif game[a][b]==7:
                 worm7.append([a,b])
@@ -127,8 +116,8 @@ for tc in range(1, T + 1):
                 worm10.append([a,b])
 
     answer=0
-    for i in range(N):
-        for j in range(N):
+    for i in range(N+2):
+        for j in range(N+2):
             if game[i][j]==0:
                 for d in range(4):
                     score=gameStart(game, i, j, d)
